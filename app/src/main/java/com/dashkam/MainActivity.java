4b7ff07c1a0d0fb7fe152359bcef4a5a7c7c0bca
Mike Wadhera <mikewadhera@gmail.com>
2016-09-27 17:11:01 -0700

    Basic activity lifecycle for camera preview

--- app/src/main/java/com/dashkam/MainActivity.java
+++ app/src/main/java/com/dashkam/MainActivity.java
@@ -1,7 +1,15 @@
 package com.dashkam;
 
+import android.Manifest;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -10,15 +18,26 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "DashKam";
+    private static int CAMERA_ID = 0;
+    private static final int PERMISSIONS_ID_CAMERA = 0;
+    private static final int PERMISSIONS_ID_GPS = 1;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -35,13 +54,49 @@ public class MainActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+    /**
+     *
+     * The underlying {@link android.graphics.Camera} owned by activity
+     *
+     */
+    public static Camera mCamera;
+
+    public static SurfaceHolder mPreviewSurfaceHolder;
+
+    public static void onPreviewReady(SurfaceHolder surfaceHolder) {
+        mPreviewSurfaceHolder = surfaceHolder;
+        // Try starting preview (if we don't have mCamera yet we'll try again in onCameraPermission)
+        startPreview();
+    }
+
+    public static void startPreview() {
+        if (mCamera != null &&
+                mPreviewSurfaceHolder != null) {
+            try {
+                mCamera.setPreviewDisplay(mPreviewSurfaceHolder);
+                mCamera.startPreview();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Check permissions
+        MainActivity.checkPermissions(this, Manifest.permission.CAMERA, PERMISSIONS_ID_CAMERA);
+        MainActivity.checkPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION, PERMISSIONS_ID_GPS);
+
+        // Try eagerly acquiring camera
+        //  - if we don't have permission we'll try again in onCameraPermission
+        acquireCamera();
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -49,73 +104,148 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        acquireCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        releaseCamera();
+    }
+
+    public void onCameraPermission() {
+        acquireCamera();
+        startPreview();
+    }
+
+    public void acquireCamera() {
+        if (mCamera == null) {
+            try {
+                mCamera = Camera.open(CAMERA_ID);
+                MainActivity.setCameraDisplayOrientation(this, CAMERA_ID, mCamera);
+                Camera.Parameters params = mCamera.getParameters();
+                params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+                mCamera.setParameters(params);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
-
+        }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_ID_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                    onCameraPermission();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            case PERMISSIONS_ID_GPS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+
+    public static class PreviewView extends SurfaceView implements SurfaceHolder.Callback {
+
+        public PreviewView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+
+            getHolder().addCallback(this);
         }
 
-        return super.onOptionsItemSelected(item);
+        public PreviewView(Context context) { super(context); }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+            MainActivity.onPreviewReady(surfaceHolder);
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+        }
+
     }
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    public static class CameraFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        public CameraFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
+        public static CameraFragment factory() {
+            return new CameraFragment();
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
+            return rootView;
+        }
+
+    }
+
+    public static class DashKamFragment extends Fragment {
+
+        public DashKamFragment() {
+
+        }
+
+        public static DashKamFragment factory() {
+            return new DashKamFragment();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_dashkam, container, false);
             return rootView;
         }
     }
@@ -132,28 +262,64 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    return CameraFragment.factory();
+                case 1:
+                    return DashKamFragment.factory();
+            }
+            return null;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Camera";
                 case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+                    return "DashKam";
             }
             return null;
         }
+    }
+
+    public static void checkPermissions(Activity context, String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(context,
+                        new String[]{permission},
+                        requestCode);
+        }
+    }
+
+    public static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
     }
 }
